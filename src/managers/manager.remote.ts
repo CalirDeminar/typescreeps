a75c2cac58f7e0c56b593607fe1cdb377f77c7ef
CalirDeminar <edward.firespinner@gmail.com>
2021-08-26 18:41:11 +0100

    remote rework

--- src/managers/manager.remote.ts
+++ src/managers/manager.remote.ts
@@ -1,5 +1,6 @@
 import { CreepBase } from "roles/role.creep";
 import { CreepBuilder } from "utils/creepBuilder";
+import { Constants } from "utils/constants";
 export class RemoteManager {
   public static getSurroundingRoomNames(room: Room): RoomPosition[] {
     const baseName = room.name;
@@ -10,18 +11,31 @@ export class RemoteManager {
       { x: 1, y: 0 },
       { x: 0, y: 1 },
       { x: 0, y: -1 }
-    ].reduce((acc: RoomPosition[], c: { x: number; y: number }) => {
-      if (currentWestString && currentNorthString) {
-        const targetRoomName = `W${parseInt(currentWestString[1]) + c.x}N${parseInt(currentNorthString[1]) + c.y}`;
-        return acc.concat([new RoomPosition(25, 25, targetRoomName)]);
-      } else {
-        return acc;
-      }
-    }, []);
+    ]
+      .reduce((acc: RoomPosition[], c: { x: number; y: number }) => {
+        if (currentWestString && currentNorthString) {
+          const targetRoomName = `W${parseInt(currentWestString[1]) + c.x}N${parseInt(currentNorthString[1]) + c.y}`;
+          // only travel to directly adjacent rooms
+          const roomRoute = Game.map.findRoute(room.name, targetRoomName);
+          if (roomRoute != -2 && roomRoute.length <= Constants.maxRemoteRoomDistance) {
+            return acc.concat([new RoomPosition(25, 25, targetRoomName)]);
+          } else {
+            return acc;
+          }
+        } else {
+          return acc;
+        }
+      }, [])
+      .sort((a, b) => {
+        const routeA = Game.map.findRoute(room.name, a.roomName);
+        const routeB = Game.map.findRoute(room.name, b.roomName);
+        const roomsToA = routeA !== -2 ? routeA.length : 100000;
+        const roomsToB = routeB !== -2 ? routeB.length : 100000;
+        return roomsToA - roomsToB;
+      });
   }
-  public static run(room: Room) {
+  public static spawnScout(room: Room) {
     const spawn = room.find(FIND_MY_SPAWNS)[0];
-    const anchor = room.find(FIND_FLAGS, { filter: (f) => f.name === `${room.name}-Anchor` })[0];
     const scouts = _.filter(Game.creeps, (c) => c.memory.role === "scout");
     const shouldSpawnScout =
       Game.time % 1500 === 0 && room.controller && room.controller.level > 1 && spawn && scouts.length === 0;
@@ -37,46 +51,40 @@ export class RemoteManager {
         }
       };
     }
-    if (Game.time % 100 === 0) {
-      let isSpawning = !!spawn.spawning;
-      const remoteSources = _.reduce(
-        Memory.roomStore[room.name].remoteRooms,
-        (acc: { source: Source; hostile: boolean; pathLength: number }[], rRoom) => {
-          const rtn = rRoom.sources.reduce((acc: { source: Source; hostile: boolean; pathLength: number }[], s) => {
-            if (s.pos) {
-              const pos = new RoomPosition(s.pos.x, s.pos.y, s.pos.roomName);
-              const path = anchor.pos.findPathTo(pos, { maxRooms: 1, ignoreCreeps: true });
-              const rtn = { source: s, hostile: rRoom.hostile, pathLength: path.length };
-              return acc.concat([rtn]);
+  }
+  public static remoteHarvest(room: Room) {
+    const remoteRooms = Memory.roomStore[room.name].remoteRooms;
+    _.map(remoteRooms, (targetRoom, targetRoomName) => {
+      if (!targetRoom.hostile && targetRoomName) {
+        const roomRoute = Game.map.findRoute(room.name, targetRoomName);
+        if (roomRoute !== -2 && roomRoute.length <= Constants.maxRemoteRoomDistance) {
+          targetRoom.sources.map((s) => {
+            const creepCount = _.filter(
+              Game.creeps,
+              (c) => c.memory.role === "harvesterShuttle" && c.memory.targetSource === s.id
+            ).length;
+            if (creepCount < Constants.maxRemoteShuttles) {
+              Memory.roomStore[room.name].nextSpawn = {
+                template: CreepBuilder.buildShuttleCreep(room.energyCapacityAvailable),
+                memory: {
+                  ...CreepBase.baseMemory,
+                  role: "harvesterShuttle",
+                  working: false,
+                  born: Game.time,
+                  targetSource: s.id,
+                  targetSourcePos: s.pos,
+                  homeRoom: room.name,
+                  targetRoom: s.pos.roomName
+                }
+              };
             }
-            return acc;
-          }, []);
-          return acc.concat(rtn);
-        },
-        []
-      ).sort((a, b) => a.pathLength - b.pathLength);
-      console.log(`Remote Source Count: ${remoteSources.length}`);
-      remoteSources.map((t) => {
-        if (
-          !isSpawning &&
-          !t.hostile &&
-          _.filter(Game.creeps, (c) => c.memory.role === "harvesterShuttle" && c.memory.targetSource === t.source.id)
-            .length < 2
-        ) {
-          const creepMemory = {
-            ...CreepBase.baseMemory,
-            role: "harvesterShuttle",
-            homeRoom: room.name,
-            targetSource: t.source.id,
-            targetSourcePos: t.source.pos
-          };
-          Memory.roomStore[room.name].nextSpawn = {
-            template: CreepBuilder.buildShuttleCreep(room.energyCapacityAvailable),
-            memory: creepMemory
-          };
-          isSpawning = true;
+          });
         }
-      });
-    }
+      }
+    });
+  }
+  public static run(room: Room) {
+    this.spawnScout(room);
+    this.remoteHarvest(room);
   }
 }
