5ba67674ef533cb835f91c21d4fb47b789134feb
Drago6777 <edward.firespinner@gmail.com>
2021-09-04 14:47:15 +0100

    WIP

--- test/unit/main.test.ts
+++ test/unit/main.test.ts
@@ -1,5 +1,6 @@
 import { assert } from "chai";
-import { UtilPosition } from "utils/util.position";
+import { CreepCombat } from "utils/creepCombat";
+// import { UtilPosition } from "utils/util.position";
 import { loop } from "../../src/main";
 import { Game, Memory } from "./mock";
 
@@ -24,21 +25,63 @@ describe("main", () => {
     assert.isUndefined(loop());
   });
 
-  it("Test util.position.getOtherSideOfExit", () => {
-    const q1 = { x: 0, y: 12, roomName: "W22N8" };
-    const a1 = { x: 49, y: 12, roomName: "W23N8" };
-    const q2 = { x: 0, y: 12, roomName: "W0N25" };
-    const a2 = { x: 49, y: 12, roomName: "W1N25" };
-    const q3 = { x: 49, y: 12, roomName: "W0N25" };
-    const a3 = { x: 0, y: 12, roomName: "E0N25" };
-    const q4 = { x: 12, y: 49, roomName: "E12N0" };
-    const a4 = { x: 12, y: 0, roomName: "E12S0" };
-    const q5 = { x: 12, y: 49, roomName: "W22N8" };
-    const a5 = { x: 12, y: 0, roomName: "W22N7" };
-    assert.deepEqual(UtilPosition.getOtherSideOfExit(q1), a1);
-    assert.deepEqual(UtilPosition.getOtherSideOfExit(q2), a2);
-    assert.deepEqual(UtilPosition.getOtherSideOfExit(q3), a3);
-    assert.deepEqual(UtilPosition.getOtherSideOfExit(q4), a4);
-    assert.deepEqual(UtilPosition.getOtherSideOfExit(q5), a5);
+  // it("Test util.position.getOtherSideOfExit", () => {
+  //   const q1 = { x: 0, y: 12, roomName: "W22N8" };
+  //   const a1 = { x: 49, y: 12, roomName: "W23N8" };
+  //   const q2 = { x: 0, y: 12, roomName: "W0N25" };
+  //   const a2 = { x: 49, y: 12, roomName: "W1N25" };
+  //   const q3 = { x: 49, y: 12, roomName: "W0N25" };
+  //   const a3 = { x: 0, y: 12, roomName: "E0N25" };
+  //   const q4 = { x: 12, y: 49, roomName: "E12N0" };
+  //   const a4 = { x: 12, y: 0, roomName: "E12S0" };
+  //   const q5 = { x: 12, y: 49, roomName: "W22N8" };
+  //   const a5 = { x: 12, y: 0, roomName: "W22N7" };
+  //   assert.deepEqual(UtilPosition.getOtherSideOfExit(q1), a1);
+  //   assert.deepEqual(UtilPosition.getOtherSideOfExit(q2), a2);
+  //   assert.deepEqual(UtilPosition.getOtherSideOfExit(q3), a3);
+  //   assert.deepEqual(UtilPosition.getOtherSideOfExit(q4), a4);
+  //   assert.deepEqual(UtilPosition.getOtherSideOfExit(q5), a5);
+  // });
+
+  it("Test Creep Calculation ", () => {
+    const heal = new Array(20).fill({ type: "heal", hits: 100, boost: "XLHO2" });
+    const movement = new Array(10).fill({ type: "move", hits: 100 });
+    const tough = new Array(10).fill({ type: "tough", hits: 100, boost: "XGHO2" });
+    const newBody = heal.concat(movement).concat(tough);
+    assert.deepEqual(CreepCombat.getCreepCombatFigures(newBody), {
+      maxEffectiveHealing: 3207,
+      toughBuffer: 3340,
+      safeBuffer: 4340,
+      dismantlePower: 0,
+      meleePower: 0,
+      rangedPower: 0
+    });
+    assert.equal(
+      CreepCombat.getCreepEffectiveHealing([
+        { type: "tough", hits: 100 },
+        { type: "tough", hits: 100 },
+        { type: "heal", hits: 100 }
+      ]),
+      12,
+      "basic components"
+    );
+    assert.equal(
+      CreepCombat.getCreepEffectiveHealing([
+        { type: "tough", hits: 100 },
+        { type: "tough", hits: 100 },
+        { type: "heal", hits: 100, boost: "XLHO2" }
+      ]),
+      48,
+      "boosted heal"
+    );
+    assert.equal(
+      CreepCombat.getCreepEffectiveHealing([
+        { type: "tough", hits: 100, boost: "XGHO2" },
+        { type: "tough", hits: 100, boost: "XGHO2" },
+        { type: "heal", hits: 100, boost: "XLHO2" }
+      ]),
+      161,
+      "boosted heal & tough"
+    );
   });
 });
