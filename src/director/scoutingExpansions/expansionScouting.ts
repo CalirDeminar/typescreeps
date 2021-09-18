368a52d6299ef498882d94459f615a289853d663
CalirDeminar <edward.firespinner@gmail.com>
2021-09-18 15:09:29 +0100

    improved expansion placement positioning

--- src/director/scoutingExpansions/expansionScouting.ts
+++ src/director/scoutingExpansions/expansionScouting.ts
@@ -1,9 +1,9 @@
-const bunkerRadius = 9;
+const bunkerRadius = 11;
 export class ExpansionScouting {
   public static isExpandableByTerrain(room: ScoutedRoom): boolean {
     const terrain = new Room.Terrain(room.name);
     if (terrain) {
-      const bounds = _.range(bunkerRadius + 1, 48 - bunkerRadius);
+      const bounds = _.range(bunkerRadius + 2, 48 - bunkerRadius);
       const offsets = _.range(0 - bunkerRadius, bunkerRadius + 1)
         .reduce((acc: { x: number; y: number }[], x: number) => {
           return acc.concat(
@@ -33,7 +33,26 @@ export class ExpansionScouting {
     return false;
   }
   public static getExpansionRoomSpawnPos(room: ScoutedRoom): RoomPosition | null {
-    const terrain = new Room.Terrain(room.name);
+    const spots = this.getExpansionSpots(room.name);
+    const rRoom = Game.rooms[room.name];
+    if (rRoom) {
+      const sources = rRoom.find(FIND_SOURCES);
+      const controller = room.controller;
+      const positions = sources.map((s) => s.pos).concat(controller ? controller.pos : []);
+      return spots.sort((a, b) => {
+        const aDistance = positions.reduce((acc: number, p: RoomPosition) => {
+          return acc + a.findPathTo(a.x, a.y, { ignoreCreeps: true, swampCost: 1 }).length;
+        }, 0);
+        const bDistance = positions.reduce((acc: number, p: RoomPosition) => {
+          return acc + a.findPathTo(b.x, b.y, { ignoreCreeps: true, swampCost: 1 }).length;
+        }, 0);
+        return aDistance - bDistance;
+      })[0];
+    }
+    return null;
+  }
+  public static getExpansionSpots(roomName: string): RoomPosition[] {
+    const terrain = new Room.Terrain(roomName);
     if (terrain) {
       const bounds = _.range(bunkerRadius + 1, 48 - bunkerRadius);
       const offsets = _.range(0 - bunkerRadius, bunkerRadius + 1)
@@ -58,11 +77,9 @@ export class ExpansionScouting {
           return terrain.get(spot.x + offset.x, spot.y + offset.y) !== 1;
         });
       });
-      if (possibleSpots.length > 0) {
-        return new RoomPosition(possibleSpots[0].x, possibleSpots[0].y, room.name);
-      }
+      return possibleSpots.map((s) => new RoomPosition(s.x, s.y, roomName));
     }
-    return null;
+    return [];
   }
   public static expandable(room: ScoutedRoom): boolean {
     const enoughSources = room.sources.length > 1;
